0a3dba0fadc82de00eb97792b01fee4bbea3e361
Armghan Saeed <armghan3071@gmail.com>
2025-04-05 09:43:54 +0200

    Naming bug fixed

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Model\Booking;
-use App\Model\Customer;
+use App\Models\Booking;
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Model\Customer;
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
